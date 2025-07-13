ecedf8708365e53b8f50b00e0de9d519696aa27d
Purabhh <156032267+Purabhh@users.noreply.github.com>
2025-07-12 21:12:49 -0400

    New Files

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import './App.css';
+
+function App() {
+  useEffect(() => {
+    document.title = "Kalonji Website";
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>Kalonji Website</h1>
+        <nav>
+          <ul>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+}
+
+export default App;
